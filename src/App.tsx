faf2e0172b7922716158f29d69db6430fef28d22
Zhafarrel-APK <zhafarrelkadarusman812@gmail.com>
2026-03-01 14:45:10 +0700

    fix typo

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,7 @@ export default function App() {
                   totalquantity
                   description
                   image {
-                    ul
+                    url
                   }
                   location
                   borrowerInfo
